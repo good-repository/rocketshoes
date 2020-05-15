7322cf879f92383e075ac784ea413642e2682868
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-15 14:59:26 -0300

    home screen styled

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 function Home() {
-  return <h1>home</h1>
+  return (
+    <ProductList>
+      <li>
+        <img
+          src='https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x'
+          alt='Tênis'
+        />
+        <strong>Tênis qualquer</strong>
+        <span>R$19,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+    </ProductList>
+  )
 }
 
 export default Home;
