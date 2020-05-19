4c79b7e01e501cbd82b9dd3286f80142c51bb142
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-19 14:15:45 -0300

    showing itens on cart page

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux'
 
 import rootReducer from './modules/rootReducer'
 
-const enhance = process.env.NODE_ENV == 'development' ? console.tron.createEnhancer() : null
+const enhance = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null
 
 const store = createStore(rootReducer, enhance);
 
