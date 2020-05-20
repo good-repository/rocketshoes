761c6e89c977ae258a2cc0a63a859c3b3dfa47b0
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-20 15:03:16 -0300

    navigation with saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects'
 import { toast } from 'react-toastify'
 
 import api from '../../../services/api'
+import history from '../../../services/history'
 import { formatPrice } from '../../../util/format'
 
 import { addToCartSuccess, updateAmountSuccess } from './actions'
@@ -22,7 +23,6 @@ function* addToCart({ id }) {
     }
 
     if (productExists) {
-
         yield put(updateAmountSuccess(id, amount))
     } else {
 
@@ -35,6 +35,7 @@ function* addToCart({ id }) {
         }
 
         yield put(addToCartSuccess(data))
+        history.push('/cart')
     }
 }
 
