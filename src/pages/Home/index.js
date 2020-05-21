fe14058982237b6a92aefd8c68364e2a2396ef17
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-05-21 12:02:33 -0300

    refactoring to use redux with hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { formatPrice } from '../../util/format'
 import api from '../../services/api'
 
@@ -10,8 +9,15 @@ import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 
-function Home({ amount, addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([])
+  const amount = useSelector(state => state.cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+
+    return sumAmount
+  }, {}))
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
     async function loadProducts() {
@@ -28,7 +34,7 @@ function Home({ amount, addToCartRequest }) {
   }, [])
 
   function handleAddProduct(id) {
-    addToCartRequest(id)
+    dispatch(CartActions.addToCartRequest(id))
   }
 
   return (
@@ -55,16 +61,3 @@ function Home({ amount, addToCartRequest }) {
     </ProductList >
   )
 }
-
-const mapStatetoProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount
-
-    return amount
-  }, {})
-})
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch)
-
-export default connect(mapStatetoProps, mapDispatchToProps)(Home)
